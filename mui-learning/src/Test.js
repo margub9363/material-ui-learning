c0d22b09681839d482bd04258b30e17f46767bc4
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-10-18 09:52:47 +0530

    Handling Forms in Material UI

--- mui-learning/src/Test.js
+++ mui-learning/src/Test.js
@@ -2,30 +2,53 @@ import React, { useState } from "react";
 import { Button, Typography, TextField } from "@mui/material";
 
 export const Test = () => {
-  const [name, setName] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <div>
-      <TextField
-        value={name}
-        onChange={(e) => setName(e.target.value + "-")}
-        sx={{ margin: 3 }}
-        type="text"
-        placeholder="Name"
-        variant="outlined"
-      />
-      <TextField
-        sx={{ margin: 3 }}
-        type="email"
-        placeholder="Email"
-        variant="standard"
-      />
-      <TextField
-        sx={{ margin: 3 }}
-        type="password"
-        placeholder="Password"
-        variant="filled"
-      />
-      <Typography>Hi {name} </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="name"
+          value={inputs.name}
+          onChange={handleChange}
+          sx={{ margin: 3 }}
+          type="text"
+          placeholder="Name"
+          variant="outlined"
+        />
+        <TextField
+          name="email"
+          value={inputs.email}
+          sx={{ margin: 3 }}
+          type="email"
+          placeholder="Email"
+          variant="standard"
+        />
+        <TextField
+          name="password"
+          value={inputs.password}
+          sx={{ margin: 3 }}
+          type="password"
+          placeholder="Password"
+          variant="filled"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <Typography>Hi {inputs.name} </Typography>
     </div>
   );
 };
